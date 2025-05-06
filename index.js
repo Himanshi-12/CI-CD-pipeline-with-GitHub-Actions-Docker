b4f81218f2310ec87a16dbf89266614196cc2cef
Himanshi Tomar <141580414+Himanshi-12@users.noreply.github.com>
2025-05-06 17:55:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const http = require("http");
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello from Node.js app!");
+});
+server.listen(3000, () => console.log("Server running on port 3000"));
